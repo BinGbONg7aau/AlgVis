fb6b7a784995d6a4e69da8cb6b25f2bc1477951f
Nikomiko <ngool23@student.aau.dk>
2026-07-13 13:59:20 +0200

    Improve readability of Mergesort

--- algs/mergeSort.c
+++ algs/mergeSort.c
@@ -1,9 +1,10 @@
 #include "mergeSort.h"
 #include "../helperFunctions.h"
 
-static void _mergeSort(int *array, int l, int r);
-static void _merge(int *array, int l, int m, int r);
+static void _mergeSort(int *array, int l, int r, int depth);
+static void _merge(int *array, int l, int m, int r, int depth);
 static void printMergeArray(int *array, int l, int r);
+static void printIndent(int depth);
 
 void mergeSort() {
     int length = -1;
@@ -15,94 +16,117 @@ void mergeSort() {
     array = generateArray(length);
     populateArrayRandom(array, length);
 
-    printf("Generated array: \n");
+    printf("Generated array:\n");
     printArray(array, length);
+    printf("\n");
 
-    _mergeSort(array, 0, length-1);
+    _mergeSort(array, 0, length - 1, 0);
+
+    printf("Sorted array:\n");
     printArray(array, length);
 
     free(array);
 }
 
-static void _mergeSort(int *array, int l, int r) {
-    
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        printf("m: %d\n", m);
-        printf("Left: ");
-        printMergeArray(array, l, m);
+static void _mergeSort(int *array, int l, int r, int depth) {
 
-        printf("Right: ");
-        printMergeArray(array, m+1, r);
+    if (l >= r)
+        return;
 
-        // Sort first and second halves
-        _mergeSort(array, l, m);
-        _mergeSort(array, m + 1, r);
+    int m = l + (r - l) / 2;
 
-        _merge(array, l, m, r);
-    }
+    printIndent(depth);
+    printf("Sorting range [%d..%d]\n", l, r);
+
+    printIndent(depth);
+    printf("Split at index %d\n", m);
+
+    printIndent(depth);
+    printf("Left  (%d..%d): ", l, m);
+    printMergeArray(array, l, m);
+
+    printIndent(depth);
+    printf("Right (%d..%d): ", m + 1, r);
+    printMergeArray(array, m + 1, r);
+
+    printf("\n");
+
+    _mergeSort(array, l, m, depth + 1);
+    _mergeSort(array, m + 1, r, depth + 1);
+
+    _merge(array, l, m, r, depth);
 }
 
-static void _merge(int *array, int l, int m, int r) {
-    
-    printf("\nMerging the following arrays now:\n");
-    printMergeArray(array, l, m);
-    printMergeArray(array, m+1, r);
+static void _merge(int *array, int l, int m, int r, int depth) {
 
-    int i, j, k;
+    printIndent(depth);
+    printf("----------------------------------------\n");
+
+    printIndent(depth);
+    printf("Merge\n");
+
+    printIndent(depth);
+    printf("Left : ");
+    printMergeArray(array, l, m);
+
+    printIndent(depth);
+    printf("Right: ");
+    printMergeArray(array, m + 1, r);
+
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
     int L[n1], R[n2];
 
-    // Copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++)
+    for (int i = 0; i < n1; i++)
         L[i] = array[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = array[m + 1 + j];
 
-    // Merge the temp arrays back into array[l..r
-    i = 0;
-    j = 0;
-    k = l;
+    for (int i = 0; i < n2; i++)
+        R[i] = array[m + 1 + i];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            array[k] = L[i];
-            i++;
-        }
-        else {
-            array[k] = R[j];
-            j++;
-        }
-        k++;
+
+        if (L[i] <= R[j])
+            array[k++] = L[i++];
+        else
+            array[k++] = R[j++];
     }
 
-    // Copy the remaining elements of L[],
-    // if there are any
-    while (i < n1) {
-        array[k] = L[i];
-        i++;
-        k++;
-    }
+    while (i < n1)
+        array[k++] = L[i++];
 
-    // Copy the remaining elements of R[],
-    // if there are any
-    while (j < n2) {
-        array[k] = R[j];
-        j++;
-        k++;
-    }
+    while (j < n2)
+        array[k++] = R[j++];
 
-    printf("Merge finished with result\n");
+    printIndent(depth);
+    printf("Result: ");
     printMergeArray(array, l, r);
-    printf("\n");
+
+    printIndent(depth);
+    printf("----------------------------------------\n\n");
 }
 
 static void printMergeArray(int *array, int l, int r) {
+
     printf("[");
-    for (; l < r; l++) {
-        printf("%d, ", array[l]);
+
+    for (int i = l; i <= r; i++) {
+
+        if (i > l)
+            printf(", ");
+
+        printf("%d", array[i]);
     }
-    printf("%d]\n", array[r]);
+
+    printf("]\n");
+}
+
+static void printIndent(int depth) {
+
+    for (int i = 0; i < depth; i++)
+        printf("    ");
 }
